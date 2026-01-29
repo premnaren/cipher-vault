0fed0ecd7a31c7744372d71f497e95a077182fce
Prem Naren <prem@example.com>
2026-01-29 12:23:01 +0530

    fixed steganography

--- models/Message.js
+++ models/Message.js
@@ -1,33 +1,15 @@
 const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  text: {
-    type: String,
-    required: true,
-  },
-  cipherType: {
-    type: String,
-    default: "none",
-  },
-  // --- NEW: SELF DESTRUCT FLAG ---
-  isBurn: {
-    type: Boolean,
-    default: false
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  text: { type: String, required: true },
+  
+  // You likely missed adding this field:
+  cipherType: { type: String, default: "plain" }, 
+  
+  isBurn: { type: Boolean, default: false },
+  timestamp: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
